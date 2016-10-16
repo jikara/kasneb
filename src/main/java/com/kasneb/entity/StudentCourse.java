edc9ba54b4460df763b6c8def719d48556ba14d2
jikara <jikara@192.168.11.103>
2016-10-16 12:02:11 +0300

    Update of student course

--- src/main/java/com/kasneb/entity/StudentCourse.java
+++ src/main/java/com/kasneb/entity/StudentCourse.java
@@ -30,12 +30,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
