ac32bb9523523f01fcf938b058cacf2138196c37
jikara <jikara@192.168.11.103>
2016-10-24 12:22:54 +0300

    Added administrator services

--- src/main/java/com/kasneb/session/PaymentFacade.java
+++ src/main/java/com/kasneb/session/PaymentFacade.java
@@ -147,26 +147,26 @@ public class PaymentFacade extends AbstractFacade<Payment> {
                 //update invoice as paid
                 invoice.setStatus(new InvoiceStatus("PAID"));
                 em.merge(invoice);
-                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Registration fee has been successfully processed", invoice.getStudentCourse().getStudent());
+                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Registration fee payment has been successfully processed", invoice.getStudentCourse().getStudent());
                 break;
             case REGISTRATION_RENEWAL_FEE:
                 //update invoice as paid
                 invoice.setStatus(new InvoiceStatus("PAID"));
                 em.merge(invoice);
-                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Registration renewal fee has been successfully processed", invoice.getStudentCourse().getStudent());
+                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Registration renewal fee payment has been successfully processed", invoice.getStudentCourse().getStudent());
                 break;
             case EXAM_ENTRY_FEE:
                 invoice.getStudentCourseSitting().setStatus(StudentCourseSittingStatus.CONFIRMED);
                 //update invoice as paid
                 invoice.setStatus(new InvoiceStatus("PAID"));
                 em.merge(invoice);
-                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Exam entry fee has been successfully processed", invoice.getStudentCourse().getStudent());
+                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Exam entry fee payment has been successfully processed", invoice.getStudentCourse().getStudent());
                 break;
             case PUBLICATION_FEE:
                 //update invoice as paid
                 invoice.setStatus(new InvoiceStatus("PAID"));
                 em.merge(invoice);
-                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Publication fee has been successfully processed", invoice.getStudentCourse().getStudent());
+                notification = new Notification(NotificationStatus.UNREAD, NotificationType.PAYMENT, "Publication fee payment has been successfully processed", invoice.getStudentCourse().getStudent());
                 break;
             default:
                 break;
