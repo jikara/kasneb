fcde5f3a902e112cc4aa1bc315994985eea9fd92
jikara <jikara@THINKTANK>
2017-05-09 17:00:14 +0300

    Sat 5/6/2107 5:27 PM

--- src/main/java/com/kasneb/api/InvoiceRest.java
+++ src/main/java/com/kasneb/api/InvoiceRest.java
@@ -7,6 +7,7 @@ package com.kasneb.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.kasneb.entity.Invoice;
 import java.util.List;
 import javax.ejb.EJB;
@@ -29,10 +30,9 @@ import javax.ws.rs.core.Response;
 @Stateless
 public class InvoiceRest {
 
-    @Context
-    private UriInfo context;
     ObjectMapper mapper = new ObjectMapper();
     Object anyResponse = new Object();
+    Hibernate5Module hbm = new Hibernate5Module();
     Response.Status httpStatus = Response.Status.INTERNAL_SERVER_ERROR;
     String json;
     @EJB
@@ -42,33 +42,34 @@ public class InvoiceRest {
      * Creates a new instance of InvoiceRest
      */
     public InvoiceRest() {
+        hbm.enable(Hibernate5Module.Feature.REPLACE_PERSISTENT_COLLECTIONS);
+        mapper.registerModule(hbm);
     }
 
     /**
      * Retrieves representation of an instance of com.kasneb.api.InvoiceRest
      *
      * @return an instance of javax.ws.rs.core.Response
+     * @throws com.fasterxml.jackson.core.JsonProcessingException
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response findAll() {
+    public Response findAll() throws JsonProcessingException {
         List<Invoice> invoices = invoiceFacade.findAll();
+        json = mapper.writeValueAsString(invoices);
         return Response
                 .status(Response.Status.OK)
-                .entity(invoices)
+                .entity(json)
                 .build();
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response findAll(@PathParam("id") Integer id) {
-        try {
-            anyResponse = invoiceFacade.find(id);
-            json = mapper.writeValueAsString(anyResponse);
-        } catch (JsonProcessingException ex) {
-            // Logger.getLogger(CountryRest.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public Response findAll(@PathParam("id") Integer id) throws JsonProcessingException {
+        Invoice invoice = invoiceFacade.find(id);
+        invoice.getInvoiceDetails().size();
+        json = mapper.writeValueAsString(invoice);
         return Response
                 .status(Response.Status.OK)
                 .entity(json)
